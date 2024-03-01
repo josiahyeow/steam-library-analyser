2f5dcda598f87c82f1f3d8b9b25a81cd415a5f35
Josiah Yeow <josiah@propps.com>
2024-03-01 18:11:48 +1100

    feat: accept props in heading components

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -1,11 +1,17 @@
-export function H1({ children }: { children: React.ReactNode }) {
+export function H1(props: React.HTMLProps<HTMLHeadingElement>) {
   return (
-    <h1 className="text-6xl font-display font-bold tracking-tighter">
-      {children}
+    <h1
+      className={`${props.className} text-6xl font-display font-bold tracking-tighter`}
+    >
+      {props.children}
     </h1>
   )
 }
 
-export function H2({ children }: { children: React.ReactNode }) {
-  return <h2 className="text-2xl font-bold">{children}</h2>
+export function H2(props: React.HTMLProps<HTMLHeadingElement>) {
+  return (
+    <h2 className={`text-2xl font-bold ${props.className}`}>
+      {props.children}
+    </h2>
+  )
 }

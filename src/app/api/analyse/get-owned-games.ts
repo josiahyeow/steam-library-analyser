80529a3a62065767a01dbe4eb27ace46f4f7ff9b
Josiah Yeow <josiah@propps.com>
2024-03-01 12:52:37 +1100

    feat: get owned games from steam api

---
+++ src/app/api/analyse/get-owned-games.ts
@@ -0,0 +1,71 @@
+import { fromUnixTime } from "date-fns"
+
+const API_BASEPATH = process.env.STEAM_API_BASEPATH
+const API_KEY = process.env.STEAM_API_KEY
+
+type OwnedGameData = {
+  appid: number
+  name: string
+  playtime_forever: number
+  img_icon_url: string
+  has_community_visible_stats: boolean
+  playtime_windows_forever: number
+  playtime_mac_forever: number
+  playtime_linux_forever: number
+  rtime_last_played: number
+  playtime_disconnected: number
+}
+
+type OwnedGamesResponse = {
+  response: {
+    game_count: number
+    games: OwnedGameData[]
+  }
+}
+
+type Game = {
+  appId: string
+  name: string
+  playtimeHours: number
+  imgIconUrl: string
+  lastPlayed: Date
+}
+
+export async function getOwnedGames({ steamId }: { steamId: string }) {
+  const endpoint = new URL("/IPlayerService/GetOwnedGames/v0001/", API_BASEPATH)
+
+  const params = new URLSearchParams({
+    key: API_KEY!,
+    steamid: steamId,
+    include_appinfo: "1",
+    format: "json",
+  })
+
+  endpoint.search = params.toString()
+
+  const response = await fetch(endpoint.toString())
+  const data: OwnedGamesResponse = await response.json()
+
+  const gameCount = data.response.game_count
+
+  const games: Game[] = data.response.games.map((game) => ({
+    appId: game.appid.toString(),
+    name: game.name,
+    playtimeHours: game.playtime_forever / 60,
+    imgIconUrl: `http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`,
+    lastPlayed: fromUnixTime(game.rtime_last_played),
+  }))
+
+  const mostPlayedGame = games.reduce((mostPlayed, game) => {
+    if (game.playtimeHours > mostPlayed.playtimeHours) {
+      return game
+    }
+    return mostPlayed
+  })
+
+  return {
+    gameCount,
+    games,
+    mostPlayedGame,
+  }
+}
--- src/app/api/analyse/route.ts
+++ src/app/api/analyse/route.ts
@@ -1,5 +1,13 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
+import { getOwnedGames } from "./get-owned-games"
 
-export async function GET(request: Request) {
-  return NextResponse.json({ message: "hello world" });
+export async function GET(request: NextRequest) {
+  const steamId = request.nextUrl.searchParams.get("steamId")
+
+  if (!steamId) {
+    return NextResponse.json({ error: "steamId is required" }, { status: 400 })
+  }
+
+  const data = await getOwnedGames({ steamId })
+  return NextResponse.json(data)
 }
